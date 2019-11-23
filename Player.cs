3b0abcd3ac67f4433870aa877018d53e61408923
Virgilio37 <44036295+Virgilio37@users.noreply.github.com>
2019-11-23 19:33:10 -0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float _speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * _speed, Input.GetAxis("Vertical") * _speed);
+    }
+}
